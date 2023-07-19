c27108c13e991ea484be635e82e0b1ffa1abb9cb
mohammedakoromah <mkoromah.yotaninspires@gmail.com>
2023-07-19 04:22:51 -0700

    _putchar

--- 0x02-functions_loops/0-putchar.c
+++ 0x02-functions_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
+
+
 /**
  * main -Entry point of my code
  *
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
--- 0x02-functions_loops/main.h
+++ 0x02-functions_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
 #endif
