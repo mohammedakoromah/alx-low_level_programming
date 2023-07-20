5b29465aeff291b23a1af663eedd6ca24388fb61
mohammedakoromah <mkoromah.yotaninspires@gmail.com>
2023-07-20 06:29:56 -0700

    I am federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-	for (b = 0; b <=9; b++)
+	for (b = 0; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
@@ -23,6 +23,7 @@ void jack_bauer(void)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
+	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
